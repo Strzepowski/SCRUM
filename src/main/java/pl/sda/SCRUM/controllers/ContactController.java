6725e944d644c472e7f35bc8d06ea9de7c5ab472
Szczepan Grzebinoga <szgrzebinoga@gmail.com>
2019-06-30 11:31:01 +0200

    PACKAGES REPAIR

--- src/main/java/pl/sda/SCRUM/controllers/ContactController.java
+++ src/main/java/pl/sda/SCRUM/controllers/ContactController.java
@@ -9,14 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PutMapping;
 
-import pl.sda.ScrumAPP.model.Contact;
-import pl.sda.ScrumAPP.repository.ContactRepository;
+import pl.sda.SCRUM.model.Contact;
+import pl.sda.SCRUM.repository.ContactRepository;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import pl.sda.ScrumAPP.service.ContactService;
+import pl.sda.SCRUM.service.ContactService;
 
 import java.util.Optional;
 

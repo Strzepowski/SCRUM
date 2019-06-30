9a8c90210f647c2a4e82a45590ea15fc383f450a
Szczepan Grzebinoga <szgrzebinoga@gmail.com>
2019-06-30 11:51:30 +0200

    index

--- src/main/java/pl/sda/SCRUM/service/ContactService.java
+++ src/main/java/pl/sda/SCRUM/service/ContactService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class ContactService {
 
-    ContactRepository contactRepository;
+    private final ContactRepository contactRepository;
 
     @Autowired
     public ContactService(ContactRepository contactRepository) {
